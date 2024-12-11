9771362df4a4090e8b4f5f8b64a3902e1097c943
akashbiju177 <akashbiju880@gmail.com>
2024-12-11 09:58:41 +0530

    modified

---
+++ web/table.php
@@ -0,0 +1,6 @@
+<?php
+$cricket=array(array('M S Dhoni',40),array('virat kohli',25),array('sachin tendulkar',45),array('Rohit sharma',35),array('sanju samson',26));
+?>
+<html>
+    <table border="1"><tr><th>Name</th><th>Age</th></tr>
+    <tr><td><?php echo $cricket[0][0]?></td><td><?php echo
